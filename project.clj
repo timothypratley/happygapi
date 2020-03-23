b845c6825e7183e6a2ad7e5d36d58027dd74c1d4
Timothy Pratley <timothypratley@gmail.com>
2020-03-22 20:23:47 -0700

    add Clojars as deploy repo

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main happy.lion
+  :deploy-repositories {"releases" {:url "https://repo.clojars.org"}}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-http "3.10.0"]
                  [cheshire "5.10.0"]]
