1fb743ec4f996d0189eae9b165493dd19ef136ce
Timothy Pratley <timothypratley@gmail.com>
2020-03-22 20:29:25 -0700

    Version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject happygapi "0.4.0-SNAPSHOT"
+(defproject happygapi "0.4.0"
   :description "A Clojure library with source generated functions for using Google APIs."
   :url "http://github.com/timothypratley/happygapi"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
