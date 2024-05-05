d1c9f133d9529810253bd6f78c2dd1454fdd2102
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2024-05-05 20:49:42 +0200

    feature: allow random port for localhost URIs

--- src/happy/oauth2_capture_redirect.clj
+++ src/happy/oauth2_capture_redirect.clj
@@ -14,19 +14,67 @@
     (catch Exception _
       nil)))
 
-(defn wait-for-redirect [{:as config :keys [redirect_uri redirect_uris]}]
+(defn redirect-uri->port
+  "Opening a browser and redirecting to a local server
+  only makes sense for localhost redirect-uris.
+
+  Known limitation: Google does not support no ipv6 [::1] with random ports"
+  [uri]
+  (let [[match protocol host _ port path] (re-find #"^(http://)(localhost|127.0.0.1)(:(\d+))?(/.*)?$" uri)
+        port (maybe-parse-int port)]
+    (if (not match)
+      (throw (RuntimeException. "URI doesn't match pattern"))
+      (or port
+          0)))) ;; Jetty's magic value 0 = random port
+
+(defn add-port
+  [uri random-port]
+  (let [[match protocol host _ port path] (re-find #"^(http://)(localhost|127.0.0.1)(:(\d+))?(/.*)?$" uri)]
+    (if (not match)
+      (throw (RuntimeException. "URI doesn't match pattern"))
+      (str protocol host ":" random-port path))))
+
+(defn query-string->params
+  "Converts an URI query-string a=b&c=d&e=f&...
+  into a param map {:a \"b\" :c \"d\" :e \"f\"}."
+  [q]
+  (let [split-by-& #(str/split % #"&")
+        split-by-= #(str/split % #"=")
+        pairs (map split-by-= (split-by-& q))
+        parameters (into {} (for [[k v] pairs]
+                              [(keyword k) v]))]
+    parameters))
+
+(defn wait-for-redirect
+  "wait-for-redirect takes a callback function and a Google client
+  config. The callback function will be called, when the jetty server
+  has been started. It receives a single argument with the effective
+  URI, i.e. the URI with the correct (maybe random) port."
+  [on-server-started-callback
+   {:as config :keys [redirect_uri redirect_uris]}]
   (let [code (promise)
-        port (or (some-> (or redirect_uri (last redirect_uris))
-                         (str/split #":")
-                         (last)
-                         (maybe-parse-int))
-                 80)
-        server (jetty/run-jetty
-                 (fn [request]
-                   (deliver code (some-> request :query-string (str/split #"=") second))
-                   {:status 200
-                    :body   "ok"})
-                 (merge {:port port :join? false} config))]
+        uri (or redirect_uri (last redirect_uris))
+        port (redirect-uri->port uri)
+        http-redirect-handler (fn [request]
+                                (deliver code (some-> request
+                                                      :query-string
+                                                      query-string->params
+                                                      :code))
+                                {:status 200
+                                 :body   "ok"})
+        server (jetty/run-jetty http-redirect-handler
+                                {:port port :join? false})
+        _ (.setStopTimeout server 1000) ;; wait 1000ms for graceful shutdown, to avoid races between stopping and returning the response
+        
+        ^org.eclipse.jetty.server.NetworkConnector
+        connector (-> server
+                      .getConnectors
+                      first)
+        effective-port (.getLocalPort connector)
+        effective-uri (if (= 0 port)
+                        (add-port uri effective-port)
+                        uri)]
+    (on-server-started-callback effective-uri)
     (-> (fn []
           (Thread/sleep 20000)
           (deliver code nil))
@@ -38,9 +86,13 @@
         (.stop server)))))
 
 (defn request-code [config scopes optional]
-  (-> (oauth2/set-authorization-parameters config scopes optional)
-      (browse/browse-url))
-  (wait-for-redirect config))
+  (let [on-server-started-callback (fn [uri]
+                                     (-> config
+                                         (dissoc :redirect_uri :redirect_uris)
+                                         (assoc :redirect_uri uri)
+                                         (oauth2/set-authorization-parameters scopes optional)
+                                         (browse/browse-url)))]
+    (wait-for-redirect on-server-started-callback config)))
 
 (defn update-credentials [config credentials scopes optional]
   ;; scopes can grow
